5e6457d3f855878383177287ce9d3d134fc9758d
MarianaAntunesJ <marianajantunes97@gmail.com>
2021-05-19 18:16:55 -0300

    Atividade 03

--- Atividade03/script.js
+++ Atividade03/script.js
@@ -1,10 +1,10 @@
-var table = document.getElementById("tabel1");
-getSum();
-
 function getSum() {
     var sumVal = 0;
-    for (var i = 1; i < table.rows.lenht; i++)
-        sumVal = sumVal + parseInt(table.rows[i].cells[2].innerHTML);
+    var elem1 = parseInt(document.getElementById('elem1').innerHTML);
+    var elem2 = parseInt(document.getElementById('elem2').innerHTML);
+    var elem3 = parseInt(document.getElementById('elem3').innerHTML);
+    var elem4 = parseInt(document.getElementById('elem4').innerHTML);
 
-    console.log("Sum =>" + sumVal);
+    sumVal = (elem1 + elem2 + elem3 + elem4);
+    document.getElementById('total').innerHTML = sumVal;
 }
